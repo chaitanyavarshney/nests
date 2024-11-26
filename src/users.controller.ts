01ec6c26697131a79f73313354a5d35c281a375d
Chaitanya Varshney <chaitanyavarshney@Chaitanyas-MacBook-Air.local>
2024-11-26 15:04:10 +0530

    res redirect headers dynamic route

--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,14 +1,24 @@
-import { Controller, Get, Req } from "@nestjs/common";
-import { Request } from "express";
+import { Controller, Get, Header, HttpCode, HttpStatus, Redirect, Req, Res } from "@nestjs/common";
+import { Request, Response } from "express";
+import { PassThrough } from "stream";
 
 @Controller('/users')
 export class UserController {
 
     @Get('/profile')
-    getProfile(@Req() req: Request) {
-        console.log(req)
+    // @Redirect('/users/wallet', 302)
+    @Header('X-Name', 'new')
+    getProfile(@Req() req: Request, @Res({passthrough: true}) res: Response) {
+        res.status(302)
+        return({
+            name: "chaitan"
+        })
+    }
+
+    @Get('/wallet')
+    getWallet(){
         return {
-            name: 'Chaitan'
+            sgr: '34'
         }
     }
 }
