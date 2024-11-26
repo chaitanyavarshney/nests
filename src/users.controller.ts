046c842d41e8a254d1bfd7a9a840d3a07f7dd2e3
Chaitanya Varshney <chaitanyavarshney@Chaitanyas-MacBook-Air.local>
2024-11-26 13:39:06 +0530

    req, controllers and routing

---
+++ src/users.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Req } from "@nestjs/common";
+import { Request } from "express";
+
+@Controller('/users')
+export class UserController {
+
+    @Get('/profile')
+    getProfile(@Req() req: Request) {
+        console.log(req)
+        return {
+            name: 'Chaitan'
+        }
+    }
+}
